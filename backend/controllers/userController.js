edf772892c1464b67487edd0e94467c8bb5a8c39
krish-bedi <93346773+krish-bedi@users.noreply.github.com>
2023-10-26 23:04:28 -0400

    Updated api
    Updated api for:
    - login
    - sign up
    - getting profile
    - updating profile (email, password, name)
    - set up cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,8 +11,22 @@ const authUser = asyncHandler(async(req, res) => {
     //res.status(401);
     //throw new Error('Something went wrong');
 
-    res.status(200).json({ message: 'Auth user' });
-})
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+});
 
 // @desc    Register a new user
 // route    POST /api/users
@@ -48,21 +62,50 @@ const registerUser = asyncHandler(async(req, res) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: 'Logout user' });
+    res.cookie('jwt', '',{
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: 'User logged out' });
 })
 
 // @desc    Get user profile
 // route    POST /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: 'User profile' });
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+    res.status(200).json(user);
 })
 
 // @desc    Update user profile
 // route    PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: 'Update user profile' });
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 })
 
 export {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,10 +7,11 @@ import {
     getUserProfile,
     updateUserProfile,
  } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
