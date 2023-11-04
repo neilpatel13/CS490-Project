ef359b827a182c1271d5d91eb8dcc1a76c9386a8
Mueen B <mueen.bung@gmail.com>
2023-11-04 00:32:29 -0400

    Not Done Testing!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import { Container } from 'react-bootstrap';
+//import Header from "./components/Header";
+//import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,11 +7,12 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <>
-    <Header />
-    <ToastContainer />
-    <Container className='my-2'>
+    {/* <Header /> */}
+      <ToastContainer />
       <Outlet />
-    </Container>
+    {/* <Container>
+
+    </Container> */}
     </>
   );
 };
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import{Link, useNavigate} from 'react-router-dom';
-import { Form, Button, Row, Col, FormLabel } from 'react-bootstrap';
-import FormContainer from '../components/FormContainer';
+import { Form, Button, FormLabel, Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRegisterMutation } from '../slices/userApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast } from 'react-toastify';
-import '../components/styles.css'
+import '../style/styles.css'
 import emailIcon from '../assets/iconEmail.svg'
 import lock from '../assets/lock.svg'
+import logo from '../assets/mainLogo.svg'
 
 const RegisterScreen = () => {
     const [name, setName] = useState('');
@@ -64,80 +64,94 @@ const RegisterScreen = () => {
       };
     
     return (
-        <FormContainer>
-            <Form.Group controlId='Sign Up'style={{color: '#000', fontFamily:'DM Sans', fontSize:'30px', fontStyle:'normal', fontWeight:700, lineHeight: 'normal'}}>
-                <FormLabel>Sign Up</FormLabel>
-            </Form.Group>
-            <Form onSubmit={submitHandler}>
-                <Form.Group className='fontStyle' controlId='name'>
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        type='name'
-                        placeholder='Enter Name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        ></Form.Control>
+        <div style={{background: '#fff', width: '1440px', height: '1024px'}} >
+            <div id='Black Box' className='blackBox'></div>
+            <div id='Crush It' style={{right:'369px',bottom:'791px',left:'253px',top:'160px',position:'absolute',color: '#fff', fontFamily:'Fredoka', fontSize:'60px', fontStyle:'normal', fontWeight: '500', lineHeight: 'normal'}}>
+                Crush It
+            </div>
+            <img className='signUpLogo'  src={logo} alt='Someone Working!'/>
+            <Container className='signUpRectangle'>
+                <Form.Group controlId='Sign Up'>
+                    <FormLabel style={{top:'40px', left: '40px', right: '497px', bottom: '825px', position: 'absolute',color: '#000', fontFamily:'DM Sans', fontSize:'30px', fontStyle:'normal', fontWeight:700, lineHeight: 'normal'}}>Sign Up</FormLabel>
                 </Form.Group>
+                <Form onSubmit={submitHandler}>
+                    <Form.Group controlId='name'>
+                        <img style={{position:'absolute', left: '40px', top:'129px', right: '588px'}} src={emailIcon}/>
+                        <Form.Label className='fontStyle' style={{position: 'absolute', left:'64px', top: '129px' }} >
+                            Name
+                        </Form.Label>
+                        <Form.Control 
+                            className='customRectangle'
+                            style={{top: '154px'}}
+                            type='name'
+                            placeholder='Enter Name'
+                            value={name}onChange={(e) => setName(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group className='fontStyle' controlId='email'>
-                    <img src={emailIcon}/>
-                    <Form.Label> Email/username</Form.Label>
-                    <Form.Control
-                        color='#1F1F1F'
-                        fontFamily='DM Sans'
-                        fontSize= '16px'
-                        fontStyle= 'normal'
-                        fontWeight= '500'
-                        lineHeight= 'normal'
-                        type='email'
-                        placeholder='Enter Email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        ></Form.Control>
-                </Form.Group>
+                    <Form.Group controlId='email'>
+                        <img style={{position:'absolute', left: '40px',top: '250px'}} src={emailIcon}/>
+                        <Form.Label className='fontStyle' style={{top: '250px', position: 'absolute', left: '64px'}} > 
+                            Email/username
+                        </Form.Label>
+                        <Form.Control
+                            style={{top: '275px'}}
+                            className='customRectangle'
+                            type='email'
+                            placeholder='Enter Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            ></Form.Control>
+                    </Form.Group>
 
-                <Form.Group className='fontStyle' controlId='password' >
-                    <img src={lock}/>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        color='#1F1F1F'
-                        fontFamily='DM Sans'
-                        fontSize= '16px'
-                        fontStyle= 'normal'
-                        fontWeight= '500'
-                        lineHeight= 'normal'
-                        type='password'
-                        placeholder='Enter Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        ></Form.Control>
-                </Form.Group>
+                    <Form.Group  controlId='password' >
+                        <img style={{position:'absolute', left: '40px', top: '371px'}} src={lock}/>
+                        <Form.Label className='fontStyle'style={{left: '64px',top: '371px',position: 'absolute'}} >
+                            Password
+                        </Form.Label>
+                        <Form.Control
+                            style={{top: '396px'}}
+                            className='customRectangle'
+                            type='password'
+                            placeholder='Enter Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            ></Form.Control>
+                    </Form.Group>
 
-                <Form.Group className='fontStyle' controlId='confirmPassword'>
-                    <img src={lock}/>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Confirm Password'
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        ></Form.Control>
-                </Form.Group>
+                    <Form.Group controlId='confirmPassword'>
+                        <img style={{position:'absolute', left: '40px', top:'492px'}} src={lock}/>
+                        <Form.Label className='fontStyle'style={{position:'absolute', top: '492px', left:'64px'}} >
+                            Confirm Password
+                        </Form.Label>
+                        <Form.Control
+                            style={{top: '521px'}}
+                            className='customRectangle'
+                            type='password'
+                            placeholder='Confirm Password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            ></Form.Control>
+                    </Form.Group>
 
-                <Button type='submit' variant='primary' className='customButton'>
-                    Sign Up
-                </Button>
+                    <Button type='submit' variant='primary' className='customButton'>
+                        Sign Up
+                    </Button>
 
-                {isLoading }
+                    {isLoading }
 
-            </Form>
+                </Form>
+                
+                <div className='customRectangle2'>
+                    <div className='fontStyle2'>
+                        Already have an account? <Link to={'/login'}>Sign in here!</Link>
+                    </div>
+                </div>
+            </Container>
             
-            <Row className='py-3'>
-                <Col>
-                    Already have an account? <Link to={'/login'}>Sign in here!</Link>
-                </Col>
-            </Row>
-        </FormContainer>
+        </div>
+
     );
 };
 export default RegisterScreen;
