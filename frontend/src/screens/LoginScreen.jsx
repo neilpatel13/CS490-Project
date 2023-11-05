6672b3f61b6a9933c1aec144d9e0754181fd72e3
smccarten <sm3252@njit.edu>
2023-11-04 23:08:38 -0400

    Added the figma styling to the Sign in Page

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, FormLabel, Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import FormContainer from '../components/FormContainer';
 import { useLoginMutation } from '../slices/userApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast } from 'react-toastify';
 import Loader from '../components/Loader';
+import '../style/styles.css'
+import emailIcon from '../assets/iconEmail.svg'
+import lock from '../assets/lock.svg'
+import logo from '../assets/mainLogo.svg'
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
@@ -38,23 +41,41 @@ const LoginScreen = () => {
     
     return (
 
-        <FormContainer>
-
-            <h1>Sign In</h1>
+        <div style={{background: '#fff', width: '1440px', height: '1024px', position: 'relative'}} >
+            <div id='Black Box' className='blackBox'></div>
+                <div id='Crush It' style={{right:'369px',bottom:'791px',left:'253px',top:'160px',position:'absolute',color: '#fff', fontFamily:'Fredoka', fontSize:'60px', fontStyle:'normal', fontWeight: '500', lineHeight: 'normal'}}>
+                    Crush It
+                </div>
+            <img className='signUpLogo'  src={logo} alt='Someone Working!'/>
+            <Container className='signUpRectangle'>
+            <Form.Group controlId='Sign In'>
+                    <FormLabel style={{top:'40px', left: '40px', right: '497px', bottom: '825px', position: 'absolute',color: '#000', fontFamily:'DM Sans', fontSize:'30px', fontStyle:'normal', fontWeight:700, lineHeight: 'normal'}}>Sign in</FormLabel>
+                </Form.Group>
             <Form onSubmit={submitHandler}>
-                <Form.Group className='my-2' controlId='email'>
-                    <Form.Label>Email Address/username</Form.Label>
+                <Form.Group controlId='email'>
+                <img style={{position:'absolute', left: '40px', top:'129px', right: '588px'}} src={emailIcon}/>
+                    <Form.Label className='fontStyle' style={{position: 'absolute', left:'64px', top: '129px' }}>
+                        Email Address/username
+                    </Form.Label>
                     <Form.Control
+                        className='customRectangle'
+                        style={{top: '154px'}}
                         type='email'
                         placeholder='Enter email'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        ></Form.Control>
+                        >
+                        </Form.Control>
                 </Form.Group>
 
-                <Form.Group className='my-2' controlId='password'>
-                    <Form.Label>Password</Form.Label>
+                <Form.Group controlId='password'>
+                <img style={{position:'absolute', left: '40px', top: '250px'}} src={lock}/>
+                    <Form.Label className='fontStyle'style={{left: '64px',top: '250px',position: 'absolute'}} >
+                        Password
+                    </Form.Label>
                     <Form.Control
+                        style={{top: '275px'}}
+                        className='customRectangle'
                         type='password'
                         placeholder='Enter Password'
                         value={password}
@@ -62,20 +83,21 @@ const LoginScreen = () => {
                         ></Form.Control>
                 </Form.Group>
 
-                <Button type='submit' variant='primary' className='mt-3'>
+                <Button type='submit' variant='primary' className='customButton'>
                     Sign in
                 </Button>
-                </Form>
-                
+
                 {isLoading && <Loader />}
 
-                <Row className='py-3'>
-                    <Col>
-                    Need An Account? <Link to={'/register'}> Sign Up</Link>
-                    </Col>
-                </Row>
-        </FormContainer>
+                </Form>
 
+                <div className='customRectangle2'>
+                    <div className='fontStyle2'>
+                    Need An Account? <Link to={'/register'}> Sign Up</Link>
+                    </div>
+                </div>
+            </Container>
+        </div>
     );
 };
 export default LoginScreen;
