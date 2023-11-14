49476adb499ad5642a41b88ce0931676a8b58aeb
krish-bedi <93346773+krish-bedi@users.noreply.github.com>
2023-11-14 17:07:34 -0500

    first

---
+++ backend/test/taskController.test.js
@@ -0,0 +1,29 @@
+const { addTask } = require('../controllers/taskController');
+
+describe('Task Controller', () => {
+  test('should create a new task', async () => {
+    // Mock request and response objects
+    const req = {
+      body: {
+        title: 'Test Task',
+        priority: 'top priority',
+        notes: 'Test notes',
+        date: new Date()
+      },
+      user: {
+        _id: 'mockUserId'
+      }
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    // Call the function
+    await addTask(req, res);
+
+    // Assertions
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith(expect.any(Object));
+  });
+});
