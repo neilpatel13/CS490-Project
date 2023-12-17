5b0d2142eb02dbffdba4ec4b34ef227359881e04
krish-bedi <93346773+krish-bedi@users.noreply.github.com>
2023-12-16 19:37:15 -0500

    Update TasksAppts.jsx

--- frontend/src/screens/TasksAppts.jsx
+++ frontend/src/screens/TasksAppts.jsx
@@ -59,8 +59,6 @@ const TasksAppts = () => {
       skip: isToday(selectedDate) && !displayCurrentDayTasks
     });
 
-    const [displayCurrentDayTasks, setDisplayCurrentDayTasks] = useState(false);
-
     const [loadCurrentDayTasks, setLoadCurrentDayTasks] = useState(false);
 
   // Function to fetch tasks based on the selected date
